28d11b0b1673365421c0209552a9f64cd6d14475
Krishna Murthy <krrish94@gmail.com>
2019-02-10 12:57:55 -0500

    Add timing capability

--- test.py
+++ test.py
@@ -1,60 +1,70 @@
 import torch
+import time
 
 
 class Test:
-    """Tests the ``model`` on the specified test dataset using the
-    data loader, and loss criterion.
+	"""Tests the ``model`` on the specified test dataset using the
+	data loader, and loss criterion.
 
-    Keyword arguments:
-    - model (``nn.Module``): the model instance to test.
-    - data_loader (``Dataloader``): Provides single or multi-process
-    iterators over the dataset.
-    - criterion (``Optimizer``): The loss criterion.
-    - metric (```Metric``): An instance specifying the metric to return.
-    - device (``torch.device``): An object representing the device on which
-    tensors are allocated.
+	Keyword arguments:
+	- model (``nn.Module``): the model instance to test.
+	- data_loader (``Dataloader``): Provides single or multi-process
+	iterators over the dataset.
+	- criterion (``Optimizer``): The loss criterion.
+	- metric (```Metric``): An instance specifying the metric to return.
+	- device (``torch.device``): An object representing the device on which
+	tensors are allocated.
 
-    """
+	"""
 
-    def __init__(self, model, data_loader, criterion, metric, device):
-        self.model = model
-        self.data_loader = data_loader
-        self.criterion = criterion
-        self.metric = metric
-        self.device = device
+	def __init__(self, model, data_loader, criterion, metric, device):
+		self.model = model
+		self.data_loader = data_loader
+		self.criterion = criterion
+		self.metric = metric
+		self.device = device
 
-    def run_epoch(self, iteration_loss=False):
-        """Runs an epoch of validation.
+	def run_epoch(self, iteration_loss=False):
+		"""Runs an epoch of validation.
 
-        Keyword arguments:
-        - iteration_loss (``bool``, optional): Prints loss at every step.
+		Keyword arguments:
+		- iteration_loss (``bool``, optional): Prints loss at every step.
 
-        Returns:
-        - The epoch loss (float), and the values of the specified metrics
+		Returns:
+		- The epoch loss (float), and the values of the specified metrics
 
-        """
-        self.model.eval()
-        epoch_loss = 0.0
-        self.metric.reset()
-        for step, batch_data in enumerate(self.data_loader):
-            # Get the inputs and labels
-            inputs = batch_data[0].to(self.device)
-            labels = batch_data[1].long().to(self.device)
+		"""
+		self.model.eval()
+		epoch_loss = 0.0
+		self.metric.reset()
+		avgTime = 0.0
+		numTimeSteps = 0
+		for step, batch_data in enumerate(self.data_loader):
+			startTime = time.time()
+			# Get the inputs and labels
+			inputs = batch_data[0].to(self.device)
+			labels = batch_data[1].long().to(self.device)
 
-            with torch.no_grad():
-                # Forward propagation
-                outputs = self.model(inputs)
+			with torch.no_grad():
+				# Forward propagation
+				outputs = self.model(inputs)
 
-                # Loss computation
-                loss = self.criterion(outputs, labels)
+				# Loss computation
+				loss = self.criterion(outputs, labels)
 
-            # Keep track of loss for current epoch
-            epoch_loss += loss.item()
+			# Keep track of loss for current epoch
+			epoch_loss += loss.item()
 
-            # Keep track of evaluation the metric
-            self.metric.add(outputs.detach(), labels.detach())
+			# Keep track of evaluation the metric
+			self.metric.add(outputs.detach(), labels.detach())
+			endTime = time.time()
+			avgTime += (endTime - startTime)
+			numTimeSteps += 1
 
-            if iteration_loss > 0 and (step % iteration_loss == 0):
-                print("[Step: %d/%d] Iteration loss: %.4f" % (step, len(self.data_loader), loss.item()))
+			if iteration_loss > 0 and (step % iteration_loss == 0):
+				print("[Step: %d/%d (%3.2f ms)] Iteration loss: %.4f" % (step, len(self.data_loader), \
+					1000*(avgTime / (numTimeSteps if numTimeSteps>0 else 1)), loss.item()))
+				numTimeSteps = 0
+				avgTime = 0.
 
-        return epoch_loss / len(self.data_loader), self.metric.value()
+		return epoch_loss / len(self.data_loader), self.metric.value()
--- train.py
+++ train.py
@@ -1,63 +1,74 @@
+import time
+
 class Train:
-    """Performs the training of ``model`` given a training dataset data
-    loader, the optimizer, and the loss criterion.
+	"""Performs the training of ``model`` given a training dataset data
+	loader, the optimizer, and the loss criterion.
 
-    Keyword arguments:
-    - model (``nn.Module``): the model instance to train.
-    - data_loader (``Dataloader``): Provides single or multi-process
-    iterators over the dataset.
-    - optim (``Optimizer``): The optimization algorithm.
-    - criterion (``Optimizer``): The loss criterion.
-    - metric (```Metric``): An instance specifying the metric to return.
-    - device (``torch.device``): An object representing the device on which
-    tensors are allocated.
+	Keyword arguments:
+	- model (``nn.Module``): the model instance to train.
+	- data_loader (``Dataloader``): Provides single or multi-process
+	iterators over the dataset.
+	- optim (``Optimizer``): The optimization algorithm.
+	- criterion (``Optimizer``): The loss criterion.
+	- metric (```Metric``): An instance specifying the metric to return.
+	- device (``torch.device``): An object representing the device on which
+	tensors are allocated.
 
-    """
+	"""
 
-    def __init__(self, model, data_loader, optim, criterion, metric, device):
-        self.model = model
-        self.data_loader = data_loader
-        self.optim = optim
-        self.criterion = criterion
-        self.metric = metric
-        self.device = device
+	def __init__(self, model, data_loader, optim, criterion, metric, device):
+		self.model = model
+		self.data_loader = data_loader
+		self.optim = optim
+		self.criterion = criterion
+		self.metric = metric
+		self.device = device
 
-    def run_epoch(self, iteration_loss=0):
-        """Runs an epoch of training.
+	def run_epoch(self, iteration_loss=0):
+		"""Runs an epoch of training.
 
-        Keyword arguments:
-        - iteration_loss (``bool``, optional): Prints loss at every step.
+		Keyword arguments:
+		- iteration_loss (``bool``, optional): Prints loss at every step.
 
-        Returns:
-        - The epoch loss (float).
+		Returns:
+		- The epoch loss (float).
 
-        """
-        self.model.train()
-        epoch_loss = 0.0
-        self.metric.reset()
-        for step, batch_data in enumerate(self.data_loader):
-            # Get the inputs and labels
-            inputs = batch_data[0].to(self.device)
-            labels = batch_data[1].long().to(self.device)
+		"""
+		self.model.train()
+		epoch_loss = 0.0
+		self.metric.reset()
+		avgTime = 0.0
+		numTimeSteps = 0
+		for step, batch_data in enumerate(self.data_loader):
+			startTime = time.time()
+			# Get the inputs and labels
+			inputs = batch_data[0].to(self.device)
+			labels = batch_data[1].long().to(self.device)
 
-            # Forward propagation
-            outputs = self.model(inputs)
+			# Forward propagation
+			outputs = self.model(inputs)
 
-            # Loss computation
-            loss = self.criterion(outputs, labels)
+			# Loss computation
+			loss = self.criterion(outputs, labels)
 
-            # Backpropagation
-            self.optim.zero_grad()
-            loss.backward()
-            self.optim.step()
+			# Backpropagation
+			self.optim.zero_grad()
+			loss.backward()
+			self.optim.step()
 
-            # Keep track of loss for current epoch
-            epoch_loss += loss.item()
+			# Keep track of loss for current epoch
+			epoch_loss += loss.item()
 
-            # Keep track of the evaluation metric
-            self.metric.add(outputs.detach(), labels.detach())
+			# Keep track of the evaluation metric
+			self.metric.add(outputs.detach(), labels.detach())
+			endTime = time.time()
+			avgTime += (endTime - startTime)
+			numTimeSteps += 1
 
-            if iteration_loss > 0 and (step % iteration_loss == 0):
-                print("[Step: %d/%d] Iteration loss: %.4f" % (step, len(self.data_loader), loss.item()))
+			if iteration_loss > 0 and (step % iteration_loss == 0):
+				print("[Step: %d/%d (%3.2f ms)] Iteration loss: %.4f" % (step, len(self.data_loader), \
+					1000*(avgTime / (numTimeSteps if numTimeSteps>0 else 1)), loss.item()))
+				numTimeSteps = 0
+				avgTime = 0.
 
-        return epoch_loss / len(self.data_loader), self.metric.value()
+		return epoch_loss / len(self.data_loader), self.metric.value()
